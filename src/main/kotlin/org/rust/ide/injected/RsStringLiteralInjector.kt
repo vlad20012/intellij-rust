c2ac4ee6f69cce5041d02f1f02c9c400e99b52d4
vlad20012 <beskvlad@gmail.com>
2020-11-06 14:21:47 +0300

    Temp

--- src/main/kotlin/org/rust/ide/injected/RsStringLiteralInjector.kt
+++ src/main/kotlin/org/rust/ide/injected/RsStringLiteralInjector.kt
@@ -5,36 +5,53 @@
 
 package org.rust.ide.injected
 
+import com.intellij.lang.Language
 import com.intellij.lang.injection.MultiHostInjector
 import com.intellij.lang.injection.MultiHostRegistrar
 import com.intellij.psi.PsiElement
 import org.intellij.lang.regexp.RegExpLanguage
 import org.rust.lang.core.psi.*
-import org.rust.lang.core.psi.ext.RsAbstractable
-import org.rust.lang.core.psi.ext.RsAbstractableOwner
-import org.rust.lang.core.psi.ext.owner
+import org.rust.lang.core.psi.ext.*
 import org.rust.lang.core.types.type
 
 /**
  * Injects RegExpr language to a string literals in context like
  * `Regex::new("...")` and `RegexSet::new(&["...", "...", "..."])`
  */
-class RsRegExpInjector : MultiHostInjector {
+class RsStringLiteralInjector : MultiHostInjector {
     override fun elementsToInjectIn(): List<Class<out PsiElement>> =
         listOf(RsLitExpr::class.java)
 
     override fun getLanguagesToInject(registrar: MultiHostRegistrar, context: PsiElement) {
         if (context.isValid && context is RsLitExpr) {
             val range = (context.kind as? RsLiteralKind.String)?.offsets?.value ?: return
-            if (shouldInject(context)) {
-                registrar.startInjecting(RegExpLanguage.INSTANCE)
-                    .addPlace(null, null, context, range)
-                    .doneInjecting()
+            for (injector in injectors) {
+                val language = injector.language ?: continue
+                if (injector.shouldInject(context)) {
+                    registrar.startInjecting(language)
+                        .addPlace(null, null, context, range)
+                        .doneInjecting()
+                }
             }
         }
     }
 
-    private fun shouldInject(context: RsLitExpr): Boolean =
+    private val injectors: List<RsStringLiteralInjectionSite> = listOf(
+        RsRegExpInjector1,
+        RsSqlInjector
+    )
+}
+
+private interface RsStringLiteralInjectionSite {
+    val language: Language?
+    fun shouldInject(context: RsLitExpr): Boolean
+}
+
+private object RsRegExpInjector1 : RsStringLiteralInjectionSite {
+    override val language: Language?
+        get() = RegExpLanguage.INSTANCE
+
+    override fun shouldInject(context: RsLitExpr): Boolean =
         isRegexNew(context) || isRegexSetNew(context)
 
     /** Regex::new("...") */
@@ -56,6 +73,27 @@ class RsRegExpInjector : MultiHostInjector {
         // use this logic for now because we can't resolve `RegexSet::new` (looks like it's implemented by a macro)
         val call = (context.parent?.parent?.parent?.parent as? RsCallExpr)?.expr as? RsPathExpr ?: return false
         return call.path.text == "RegexSet::new"
+    }
+}
+
+private object RsSqlInjector : RsStringLiteralInjectionSite {
+    override val language: Language?
+        get() = Language.findLanguageByID("SQL")
+
+    override fun shouldInject(context: RsLitExpr): Boolean {
+        val tt = context.parent as? RsMacroArgumentTT ?: return false
+        val argument = tt.parent as? RsMacroArgument ?: return false
+
+        if (tt.childrenOfType<RsLitExpr>().getOrNull(0) != context) return false
+
+        val macroCall = argument.parent as? RsMacroCall ?: return false
+
+        when (macroCall.macroName) {
+            "query", "query_unchecked", "query_as", "query_as_unchecked" -> Unit
+            else -> return false
+        }
+
+        return macroCall.resolveToMacro()?.containingCargoPackage?.name == "sqlx"
     }
 }
 
